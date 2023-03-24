fce66cdc0d8592da2babc7558aebecc87141cdb1
Zsolt Ero <zsolt.ero@gmail.com>
2023-03-24 19:07:08 +0100

    work

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -2,9 +2,14 @@
 
 set -eo pipefail
 
-touch brev-user-started
+touch "brev-user-started-$(date +%H%M%S)"
 
 
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe"
 sudo apt-get update && sudo apt-get install -y git zip make build-essential
 
+# shell to bash
+sudo chsh -s /bin/bash ubuntu
+
+
+touch "brev-user-finished-$(date +%H%M%S)"
